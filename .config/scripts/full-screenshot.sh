44338c76b3812a7cad6600e87ddc656824f83ad4
Pako3549 <bellarosa.pasquale@gmail.com>
2025-06-29 19:47:50 +0200

    chore(full-screenshot.sh): handle exception when user presses "Esc" key

--- .config/scripts/full-screenshot.sh
+++ .config/scripts/full-screenshot.sh
@@ -6,15 +6,17 @@
 
 dir="/home/pako/Immagini/screenshots/"
 
-hyprshot -m output -o "$dir" --silent
+if hyprshot -m output -o "$dir" --silent; then
+	latest_file=$(ls -t "$dir"*_hyprshot.png 2>/dev/null | head -n1)
+	
+	if [ -n "$latest_file" ] && [ -f "$latest_file" ]; then
+		wl-copy < "$latest_file"
 
-latest_file=$(ls -t "$dir"*_hyprshot.png 2>/dev/null | head -n1)
-
-wl-copy < "$latest_file"
-
-notify=$(dunstify -a System -u "low" --action="default, open_image" "Screenshot copied and saved" -t 2500 -h string:x-dunst-stack-tag:color-picker)
-	[ "$notify" = "default" ] && {
-		nautilus "file://$latest_file"
-		dunstify -a System -u normal "Opening screenshot..." -t 1500 -h string:x-dunst-stack-tag:color-picker
-	}
+		notify=$(dunstify -a System -u "low" --action="default, open_image" "Screenshot copied and saved" -t 2500 -h string:x-dunst-stack-tag:color-picker)
+			[ "$notify" = "default" ] && {
+				nautilus "file://$latest_file"
+				dunstify -a System -u normal "Opening screenshot..." -t 1500 -h string:x-dunst-stack-tag:color-picker
+			}
+	fi
+fi
 
