7e125cb7e089bba483ca864687cd811f0b62f4aa
Pako3549 <bellarosa.pasquale@gmail.com>
2025-07-17 21:23:09 +0200

    chore: remove unnecessary .config dirs and update fish environmental variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,14 +7,18 @@ fish_add_path ~
 ###----- ALIASES -----###
 alias vim=nvim
 
-###----- FUNCTIONS -----###
-function fish_greeting
-    echo 'Welcome'(set_color 0000CD) $USER (set_color FFFFFF)'(≧◡≦)'
-end
+###----- THEMES ENV -----###
+set -gx GTK_THEME Adwaita:dark
+set -gx QT_QPA_PLATFORMTHEME gtk3
+set -gx QT_STYLE_OVERRIDE Adwaita-Dark
 
-# pnpm
+###----- PNPM HOME -----#
 set -gx PNPM_HOME "/home/pako/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
-# pnpm end
+
+###----- FUNCTIONS -----###
+function fish_greeting
+    echo 'Welcome'(set_color 0000CD) $USER (set_color FFFFFF)'(≧◡≦)'
+end
